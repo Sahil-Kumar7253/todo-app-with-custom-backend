5cb9c7b8cbeabbf3a9cf239642fb19fb79e4eda7
Sahil-Kumar7253 <darkslayer7253@gmail.com>
2025-07-22 12:18:48 +0530

    For Render Deployment

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const app = require("./app");
 const db = require("./config/mogooseConnection");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port : http://localhost:${port}`);
